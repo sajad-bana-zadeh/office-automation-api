7c1ab592b73d8e8a52a2f62387c7183f86dafc8d
Sajad Bana Zadeh <sajadbanazadeh@gmail.com>
2026-07-07 00:46:15 +0330

    make swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe(
+      {
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Office Automation API')
+    .setDescription('REST API for office automation system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get('')
+    public findAll(){
+        return [
+            {
+                id: 1,
+                fullName: 'Sajad',
+                role: 'admin',
+            },
+        ];
+    }
+}
